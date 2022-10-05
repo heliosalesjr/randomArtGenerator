55ee46dff1cb4a72e3c79005e85ba1f36b5125bb
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2022-10-05 17:40:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let x, y;
+let speedX, speedY;
+let redVal, greenVal, blueVal;
+let alphaVal = 70;
+let angle = 0;
+
+function setup() {
+  // TODO: Create canvas and set initial background color
+  createCanvas(windowWidth,windowHeight);
+  background(50);
+  // TODO: Randomize variables used to draw shapes
+  x = random(0, width);
+  y = random(0, height);
+
+  speedX = random(-3,3);
+  speedY = random(-3,3);
+}
+
+function draw() {
+  // TODO: Map key inputs to various shape drawing functions
+  if(keyIsPressed = true && key != ' ') {
+    if(keyCode % 5 === 0) {
+      parametricLines();      
+    } else if (keyCode % 5 ===1) {
+      bouncingEllipse(random(-100,50));
+    } else if (keyCode % 5 === 2) {
+      flower();
+    }
+  }
+
+}
+
+// TODO: Add keyPressed() function to ensure that the drawing functions don't repeat on single key presses
+
+let keyPressed = function() {
+  if (key != ' ') {
+    if(keyCode % 5 === 3) {
+      roundedRect();
+    } else if (keyCode % 5 === 4) {
+      star(random(30,50), random(30,50), random(4,7));
+    } else if (keyCode === 13) {
+      saveCanvas('canvas - ' + frameCount, 'png');
+    }
+  } else {
+    clear();
+    background(50);
+
+  }
+}
+
+// TODO: Reset canvas on spacebar press
+// TODO: Save image on Return/Enter press
+// TODO: Add keyReleased() function to randomize values again on key release
+
+function keyReleased() {
+  x = random(0,width);
+  y = random(0, height);
+  speedX = random(-3,3);
+  speedY = random(-3,3);
+}
